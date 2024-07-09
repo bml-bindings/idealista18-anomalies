2ba211486f3c9f2a7bc6679cd572ac0de1116a6c
Mercè Martín Prats <merce@bigml.com>
2024-07-09 16:45:36 +0200

    Changing data path to match deployment location

--- ui/src/idealista18_anomalies/config.py
+++ ui/src/idealista18_anomalies/config.py
@@ -15,8 +15,8 @@ STREAMLIT_LOGO = "https://github.com/unmonoqueteclea/valencianow/blob/main/ui/re
 BIGML_LOGO = "https://static.bigml.com/static/images/brand_guidelines/bigml_primary_logo@2x.png"
 MAPBOX_LOGO = "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/mapbox-logo-icon.png"
 # data sources
-print(Path(__file__).parents[1])
-DATA_PATH = Path(__file__).parents[1] / 'data'
+print(Path(__file__).parents[2])
+DATA_PATH = Path(__file__).parents[2] / 'data'
 
 DEFAULT_FIELDS = ["LONGITUDE", "LATITUDE"]
 
